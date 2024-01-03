29475130d2f0e5ff0f4adccb7d99b36f66b4f277
Проходная <nikolayromanchuk88@gmail.com>
2024-01-03 23:53:31 +0300

    03_01_24

---
+++ src/main/separator11/screen3.jsx
@@ -0,0 +1,38 @@
+import "./screen3.css"
+
+function MainThirdScreen () {
+
+return(
+  
+  <div className="screen3">
+    <div className="eleven">
+    <section className="section section-white-t">
+        Светлая секция
+    </section>    
+    <div className="separator-color">
+        <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="60" viewBox="0.1 0.3 200 19" preserveAspectRatio="none">
+            <g transform="translate(-0.21755166,-100.15454)">
+                <path style="fill:#FFF;" d="M 0.2688579,100.29477 H 200.98548 c 0,0 -99.37375,39.84098 -200.7166221,0 z" />
+            </g>
+        </svg>
+    </div>
+    <section className="section section-color">
+        Темная секция    
+    </section>    
+    <div className="separator-white">
+        <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="60" viewBox="0.1 0.3 200 19" preserveAspectRatio="none">
+            <g transform="translate(-0.21755166,-100.15454)">
+                <path style="fill:#337AB7;" d="M 0.2688579,100.29477 H 200.98548 c 0,0 -99.37375,39.84098 -200.7166221,0 z" />
+            </g>
+        </svg>
+    </div>
+    <section className="section section-white-b">
+        Светлая секция
+    </section>
+</div>
+    
+  </div>
+
+)}
+
+export default MainThirdScreen;   
